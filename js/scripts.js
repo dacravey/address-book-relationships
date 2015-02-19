7df34d16ca2252d5c746d09be747f288c510b5f7
Debbie Cravey and Arsi <cinemaestro@yahoo.com>
2015-02-19 11:52:49 -0800

    revise to allow user to add more than one address.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,45 @@
 $(document).ready(function() {
+  $("#add-address").click(function() {
+
+    $("#new-addresses").append('<div class="new-address">' +
+                                  '<div class="form-group">' +
+                                      '<label for="new-street">Street</label>' +
+                                      '<input type="text" class="form-control new-street">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-city">City</label>' +
+                                    '<input type="text" class="form-control new-city">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-state">State</label>' +
+                                    '<input type="text" class="form-control new-state">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                    '<label for="new-zip">Zip</label>' +
+                                    '<input type="text" class="form-control new-zip">' +
+                                  '</div>');
+  });
+
+
   $('form#new-contact').submit(function(event) {
     event.preventDefault();
 
     var inputFirstName = $("input#new-first-name").val();
     var inputLastName = $("input#new-last-name").val();
 
-    var inputStreet = $("input#new-street").val();
-    var inputCity = $("input#new-city").val();
-    var inputState = $("input#new-state").val();
-    var inputZip = $("input#new-zip").val();
-    var newAddress = { street: inputStreet, city: inputCity, state: inputState, zip: inputZip,
-                       fullAddress: function() {
-                         return this.street + ", " + this.city + ", " + this.state + ", " + this.zip;
-                       }
-                      };
     var newContact = {firstName: inputFirstName,
-                      lastName: inputLastName, address: newAddress.fullAddress()};
+                      lastName: inputLastName, addresses: [] };
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedZip = $(this).find("input.new-zip").val();
+
+      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState, zip: inputtedZip };
+      newContact.addresses.push(newAddress);
+    });
+
 
     $('ul#contacts').append("<li><span class ='contact'>" +
                             newContact.firstName + " " +
@@ -34,7 +58,13 @@ $(document).ready(function() {
                                 newContact.lastName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+
+      $("ul#addresses").text("");
+
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " +
+        address.city + ", " + address.state + " " + address.zip + "</li>");
+      });
 
     });
   });
